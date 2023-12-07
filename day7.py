fb10afd03cce34242b386bc420215b6c42873345
Daniel Kurzynski <daniel.kurzynski@sap.com>
2023-12-07 14:20:34 +0100

    Refactor - Day 7

--- day7.py
+++ day7.py
@@ -7,26 +7,8 @@ data = get_data(day=7, year=2023)
 bids = {cards: int(bid) for cards, bid in (h.split() for h in data.splitlines())}
 hands = list(bids.keys())
 
-def classify_hand(cards):
-    card_counts = sorted(Counter(cards).values(), reverse=True)
-
-    if card_counts == [5]:
-        return 6  # Five of a kind
-    elif card_counts == [4, 1]:
-        return 5  # Four of a kind
-    elif card_counts == [3, 2]:
-        return 4  # Full house
-    elif card_counts == [3, 1, 1]:
-        return 3  # Three of a kind
-    elif card_counts == [2, 2, 1]:
-        return 2  # Two pair
-    elif card_counts == [2, 1, 1, 1]:
-        return 1  # One pair
-    else:
-        return 0  # High card
-
 def sorting_key(cards):
-    return classify_hand(cards), cards.translate(str.maketrans("TQKA", "AXYZ"))
+    return sorted(Counter(cards).values(), reverse=True), cards.translate(str.maketrans("TQKA", "AXYZ"))
 
 print(f"Part 1 - Total winnings: {sum([rank * bids[cards] for (rank, cards) in enumerate(sorted(hands, key=sorting_key), 1)])}")
 
@@ -36,6 +18,6 @@ def sorting_key_joker(cards):
         most_common_card, _ = Counter(cards.replace("J", "")).most_common(1)[0]
         best_hand = cards.replace("J", most_common_card)
 
-    return classify_hand(best_hand), cards.translate(str.maketrans("TQKA", "AXYZ")).replace("J", "1")
+    return sorted(Counter(best_hand).values(), reverse=True), cards.translate(str.maketrans("TQKA", "AXYZ")).replace("J", "1")
 
 print(f"Part 2 - Total winnings: {sum([rank * bids[cards] for (rank, cards) in enumerate(sorted(hands, key=sorting_key_joker), 1)])}")
